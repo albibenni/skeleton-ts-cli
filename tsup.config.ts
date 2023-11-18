e83700b8d391b384a04bf383433854760f710665
Alberto Benatti <albi.benni8@gmail.com>
2023-11-18 17:54:19 +0100

    tsup and vitest

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig(async () => {
+  return [
+    {
+      entry: ["packages/**/*.ts"],
+      format: ["esm", "cjs"],
+      dts: true,
+    },
+  ];
+});
